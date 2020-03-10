730db25e93686ae6b566fc7621739cc7900cc8fc
shanshuirenjia <978987373@qq.com>
2020-03-10 10:31:05 +0800

    add child compoent test

--- src/locals/en.js
+++ src/locals/en.js
@@ -1,6 +1,8 @@
 const en = {
   "shanshui": "english",
-  "hello_someone": "hello,{name}!"
+  "hello_someone": "hello,{name}!",
+  "test_child_component": "test child component",
+  "test_child_component_params": "test child component, {params}"
 };
 
 export default en;
