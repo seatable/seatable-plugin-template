c1acd0a5b464bac7e2f13eb2f711d8b04fe2601a
shanshuirenjia <978987373@qq.com>
2021-01-23 11:43:01 +0800

    update settings

--- src/setting.js
+++ src/setting.js
@@ -1,10 +1,10 @@
 
 const config = {
-  APIToken: "9851cecfd013a833eec47b629c72b3b593f91c7d",
-  server: "https://dev.seatable.cn",
-  workspaceID: "8",
-  dtableName: "plugins",
-  lang: "zh-cn"
+  APIToken: "0b2ccf4a292aa4a898969d05fc64b8a037b80531",
+  server: "http://127.0.0.1:80/",
+  workspaceID: "1",
+  dtableName: "test",
+  lang: "en"
 };
 
 const dtablePluginConfig = Object.assign({}, config, {server: config.server.replace(/\/+$/, "")}) ;
