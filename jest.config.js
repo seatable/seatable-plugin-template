eb1e39c2e7c0b6ce1c36434b5c44c1249c730d3c
Jerry Ren <rj.aiyayao@gmail.com>
2023-12-12 11:36:09 +0800

    refactor: upgrade webpack5 (#57)

---
+++ jest.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  roots: [
+    '<rootDir>/tests/',
+  ],
+  collectCoverageFrom: [
+    'tests/**/*.{js,jsx,ts,tsx}',
+    '!tests/**/*.d.ts',
+  ],
+  setupFiles: [
+    'react-app-polyfill/jsdom',
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/tests/setupTests.js',
+  ],
+  testMatch: [
+    '<rootDir>/tests/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/tests/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/config/jest/babelTransform.js',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  modulePaths: [],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^axios$': require.resolve('axios'),
+  },
+  moduleFileExtensions: [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  resetMocks: true,
+};
