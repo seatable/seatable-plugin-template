62c8ae3d468b872b12120ebed8ab3de838cc35f8
Michael An <2331806369@qq.com>
2024-02-19 18:32:30 +0800

    add languages

--- src/locale/index.js
+++ src/locale/index.js
@@ -2,12 +2,19 @@ import intl from 'react-intl-universal';
 import de from './lang/de';
 import en from './lang/en';
 import fr from './lang/fr';
+import fr from './lang/fr';
+import es from './lang/es';
+import ru from './lang/ru';
+import pt from './lang/pt';
 import zh_CN from './lang/zh_CN';
 
 const LOCALES = {
   'de': de,
   'en': en,
   'fr': fr,
+  'es': es,
+  'ru': ru,
+  'pt': pt,
   'zh-cn': zh_CN,
 };
 
