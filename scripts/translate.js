f5c4f56a1f001d15d08d6149ea9adca76b846b82
Michael An <2331806369@qq.com>
2024-02-20 09:46:58 +0800

    add languages (#59)

--- scripts/translate.js
+++ scripts/translate.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const sourcePath = path.resolve(__dirname, '../public/locale');
 const targetPath = path.resolve(__dirname, '../src/locale/lang');
 
-const SUPPORT_LANGUAGES = ['en', 'de', 'fr', 'zh_CN'];
+const SUPPORT_LANGUAGES = ['en', 'es', 'de', 'fr', 'ru', 'zh_CN', 'pt'];
 const files = fs.readdirSync(path.join(sourcePath, SUPPORT_LANGUAGES[0]));
 const pluginName = files[0];
 
@@ -17,13 +17,11 @@ export default ${lang};`
 }
 
 SUPPORT_LANGUAGES.forEach(lang => {
-  // 读出
   const contentPath = path.join(sourcePath, lang, pluginName);
   const isExist = fs.existsSync(contentPath);
   if (!isExist) return;
   const content = fs.readFileSync(contentPath, 'utf-8');
 
-  // 写入
   const langPath = path.join(targetPath, `${lang}.js`);
   const langContent = generatorLanguage(lang, content);
   fs.writeFileSync(langPath, langContent);
---
+++ src/locale/lang/pt.js
@@ -0,0 +1,8 @@
+const de = {
+  "international_demo": "international demo",
+  "shanshui": "english",
+  "hello_someone": "hello,{name}!",
+  "contans_html_params": "test child component <span class='bg-primary'>{params}</span>"
+};
+
+export default de;
